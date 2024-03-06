1fab0a033dfcff86195b6cad27b84fcb2c2eb13d
Daniel Jutila <daniel.jutila@gmail.com>
2024-03-06 14:26:36 -0600

    Fixed app.post and saving notes to database

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
-const notesRouter = require('./routes/notes.js');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/notes', notesRouter);
-app.use('/api', api);
-
 app.use(express.static('public'));
 
 // GET Route for homepage
@@ -19,9 +15,50 @@ app.get('/', (req, res) =>
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, './db/notes.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to read notes' });
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
+const { v4: uuidv4 } = require('uuid');
+
+
+app.post('/api/notes', (req, res) => {
+
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+  
+  fs.readFile(path.join(__dirname, './db/notes.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to read notes' });
+    } else {
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+
+      fs.writeFile(path.join(__dirname, './db/notes.json'), JSON.stringify(notes, null, 2), (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Failed to save note' });
+        } else {
+          res.json(newNote);
+        }
+      });
+    }
+  });
+});
